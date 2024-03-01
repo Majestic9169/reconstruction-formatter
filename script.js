88864f34ea3852886eac8d20b455b5ad77ec2a61
Majestic9169 <139123892+Majestic9169@users.noreply.github.com>
2024-03-01 19:47:26 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,16 @@
-// COOKIE FUNCTIONS START
-
-
-function setCookie(name, value, days){
+// COOKIE FUNCTIONS 
+function setCookie(name, value, days) {
     const date = new Date();
-    date.setTime(date.getTime() + days*24*60*60*1000);
+    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
     let expires = `expires=${date.toUTCString}`;
     document.cookie = `${name}=${value}; ${expires};`;
 }
-
-
-function deleteCookie(name){
+function deleteCookie(name) {
     setCookie(name, null, -100);
 }
-
-
-function getCookie(name){
-    const cookieArray = decodeURIComponent(document.cookie).split('; ');   
-    for(let i = 0; i < cookieArray.length; i++){
+function getCookie(name) {
+    const cookieArray = decodeURIComponent(document.cookie).split('; ');
+    for (let i = 0; i < cookieArray.length; i++) {
         let cookie = cookieArray[i].split(`=`);
         if (cookie[0] == name) {
             return cookie[1];
@@ -24,30 +18,43 @@ function getCookie(name){
     }
 }
 
-
-// ----------------------
 // SOLVE DATA GENERATING FUNCTIONS
+// clean up entire text area and do everything else too
 function dumpSplitter() {
-    let csTimersolves = document.getElementById('cstimer-dump').value.split('\n');
+    let csTimersolves = document.getElementById('cstimer-dump').value.trim();
+    csTimersolves = csTimersolves.split("\n");
+    csTimersolves.push("");
     let solves = [];
     for (let i = 0; i < csTimersolves.length - 1; i++) {
         csTimersolves[i] = csTimersolves[i].split('@');
         solves[i] = solveSplitter(csTimersolves[i]);
         console.log(solves[i]);
     }
-    // creating cookies
+    // creating cookies for solves (their times, scrams, and index) I AM NO LONGER CREATING INDIVIDUAL COOKIES, INSTEAD DIRECTLY USING THE RECONSTRUCTION OBJECT
     setCookie("fps", parseFloat(document.getElementById("fps").value));
-    for (let i = 0; i < solves.length; i++){
-        storeSolve(i, solves[i]);
-        console.log(getCookie(`solve${i}Time`), getCookie(`solve${i}Scram`));
-    }
-    // create pages
-
+    console.log(solves);
+    // CREATING THE RECOSNTRUCTION OBJECT
+    let recon = {};
+    recon.solver = document.getElementById("solver").value;
+    recon.reconstructor = document.getElementById("reconstructor").value;
+    recon.event = document.getElementById("event").value;
+    recon.competition = document.getElementById("competition").value;
+    recon.solves = solves;
+    console.log(recon);
+    // we need to create a cookie for number of solves, and link to the next page
+    let links = "";
+    links = `<a href="solve.html" target="_blank">GO</a>` + '\n' + '<br>' + '\n';
+    setCookie("reconstruction", JSON.stringify(recon), 1);
+    document.getElementById("link-gen").innerHTML = links;
 }
 
-
+// clean up each lines' input
 function solveSplitter(entireSolve) {
+    // entireSolve looks like 1. 7.45   D' U2 R2 B D2 B L2 B2 U2 L2 F L2 D2 R D L' D2 L' R' U'   @2024-02-28 23:24:21
     let solve = entireSolve.toString().split('   ', 2);
+    // solve looks like (only two strings are stored)
+    // solve[0] = "1. 7.45"
+    // solve[1] = "D' U2 R2 B D2 B L2 B2 U2 L2 F L2 D2 R D L' D2 L' R' U'"
     let temp = solve[0].split(' ');
     solve[0] = temp[1].trim();
     solve[1] = solve[1].trim();
@@ -55,17 +62,25 @@ function solveSplitter(entireSolve) {
     return solve;
 }
 
-// ---------------------------------
-// SOLVE COOKIES GENERATOR
-function storeSolve(index, solve){
-    setCookie(`index${index}`, parseInt(index) + 1, 1);
-    setCookie(`solve${index}Time`, solve[0], 1);
-    setCookie(`solve${index}Scram`, solve[1], 1);
+// RESET INPUT BUTTON
+function reset() {
+    let fp = document.getElementById("fps");
+    let ta = document.getElementById("cstimer-dump");
+    document.getElementById("reconstructor").value = "";
+    document.getElementById("solver").value = "";
+    document.getElementById("event").value = "";
+    document.getElementById("competition").value = "";
+    console.log(ta.value);
+    ta.value = '';
+    console.log(fp.value);
+    fp.value = '';
 }
-// -----------------------
 
 // EVENT LISTENERS
 document.getElementById("submit-cstimer-dump").addEventListener('click', dumpSplitter);
-// ---------------
+document.getElementById("reset").addEventListener('click', reset)
+document.getElementById("make-dump").addEventListener('click', function () {
+    document.getElementById("show-dump").style.display = "";
+})
 
 console.log(document.cookie);
